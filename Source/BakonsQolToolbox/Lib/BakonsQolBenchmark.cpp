7d582eed0d3ffdff6e43fc66728b31a300f585ab
BakonGuy <Bakon56@gmail.com>
2024-03-26 21:44:42 -0500

    -Add benchmark tool

---
+++ Source/BakonsQolToolbox/Lib/BakonsQolBenchmark.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright 2023-2024 Overtorque Creations LLC. All Rights Reserved. Unauthorized copying of this file, via any medium is strictly prohibited
+
+
+#include "BakonsQolBenchmark.h"
+
+void UBakonsQolBenchmark::Initialize(FSubsystemCollectionBase& Collection) {}
+void UBakonsQolBenchmark::Deinitialize() {}
+
+void UBakonsQolBenchmark::StartBenchmark()
+{
+	BenchStartTime = FPlatformTime::Seconds();
+}
+
+void UBakonsQolBenchmark::EndBenchmark(FString& ResultString)
+{
+	const double BenchEndTime = FPlatformTime::Seconds();
+	BenchResult = BenchEndTime - BenchStartTime;;
+
+	ResultString = GetPrettyBenchmarkResult();
+}
+
+double UBakonsQolBenchmark::GetBenchmarkResult() const
+{
+	return BenchResult;
+}
+
+FString UBakonsQolBenchmark::GetPrettyBenchmarkResult() const
+{
+	// Figure the best unit for result to be displayed (us, ms, or s)
+	FString Unit = "us";
+	double Multiplier = 1000000.0f;
+
+	if( BenchResult >= 0.001 ) // Milliseconds
+	{
+		Unit = "ms";
+		Multiplier = 1000.0f;
+	}
+	else if( BenchResult >= 1.0f ) // Seconds
+	{
+		Unit = "s";
+		Multiplier = 1.0f;
+	}
+
+	return FString::SanitizeFloat(BenchResult * Multiplier) + " " + Unit;
+}
---
+++ Source/BakonsQolToolbox/Lib/BakonsQolBenchmark.h
@@ -0,0 +1,36 @@
+﻿// Copyright 2023-2024 Overtorque Creations LLC. All Rights Reserved. Unauthorized copying of this file, via any medium is strictly prohibited
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "BakonsQolBenchmark.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BAKONSQOLTOOLBOX_API UBakonsQolBenchmark : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Bakons Qol Toolbox", meta = (Keywords = "Bakon Qol Toolbox"))
+	void StartBenchmark();
+
+	UFUNCTION(BlueprintCallable, Category = "Bakons Qol Toolbox", meta = (Keywords = "Bakon Qol Toolbox"))
+	void EndBenchmark(FString& ResultString);
+
+	UFUNCTION(BlueprintCallable, Category = "Bakons Qol Toolbox", meta = (Keywords = "Bakon Qol Toolbox"))
+	double GetBenchmarkResult() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Bakons Qol Toolbox", meta = (Keywords = "Bakon Qol Toolbox"))
+	FString GetPrettyBenchmarkResult() const;
+
+private:
+	double BenchStartTime;
+	double BenchResult;
+};
